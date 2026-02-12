7192088d5704ebd0335e29161128d28bf8536b11
ls4400-stack <ls4400@srmist.edu.in>
2026-02-12 18:05:34 +0530

    Coded UC1 to Print OOPS to Console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,4 +1,17 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC1: Print literal text "OOPS" to the console.
+ *
+ * @author ls4400-stack
+ * @version 1.0
+ */
 public class OOPSBannerApp {
+
+    /**
+     * Entry point of the application
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
