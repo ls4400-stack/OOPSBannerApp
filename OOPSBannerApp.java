6ccd02c8ba3c18cb11a0a2e91f931a5cfa6b0057
ls4400-stack <ls4400@srmist.edu.in>
2026-02-24 09:02:29 +0530

    UC4: Refactored banner using array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -26,13 +26,20 @@ public class OOPSBannerApp {
         String S6 = "       * ";
         String S7 = " ******  ";
 
-        System.out.println(String.join(" ", O1, O1, P1, S1));
-        System.out.println(String.join(" ", O2, O2, P2, S2));
-        System.out.println(String.join(" ", O3, O3, P3, S3));
-        System.out.println(String.join(" ", O4, O4, P4, S4));
-        System.out.println(String.join(" ", O5, O5, P5, S5));
-        System.out.println(String.join(" ", O6, O6, P6, S6));
-        System.out.println(String.join(" ", O7, O7, P7, S7));
+        // Create String array for banner lines
+        String[] banner = new String[7];
 
+        banner[0] = String.join(" ", O1, O1, P1, S1);
+        banner[1] = String.join(" ", O2, O2, P2, S2);
+        banner[2] = String.join(" ", O3, O3, P3, S3);
+        banner[3] = String.join(" ", O4, O4, P4, S4);
+        banner[4] = String.join(" ", O5, O5, P5, S5);
+        banner[5] = String.join(" ", O6, O6, P6, S6);
+        banner[6] = String.join(" ", O7, O7, P7, S7);
+
+        // Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
